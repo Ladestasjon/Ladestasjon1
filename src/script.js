34c5f6f7fc5a298021e30f693efddd67ea2d1e23
s157950 <s157950@stud.hioa.no>
2014-03-29 11:36:53 +0100

    mapq

--- src/script.js
+++ src/script.js
@@ -1,4 +1,5 @@
 
+
 //map function
 var map;
 function initialize() {
@@ -13,3 +14,24 @@ function initialize() {
 google.maps.event.addDomListener(window, 'load', initialize);
 
 //end map
+
+
+var ladestasjonApp = angular.module('ladestasjon1', []);
+
+ladestasjonApp.controller('LadestasjonReq', function($scope)
+{
+    $scope.ladestasjoner=
+            Query.ajax({
+    type: 'POST', url: 'http://nobil.no/api/server/search.php',
+    data: {'apikey': nobilApiKey, 'apiversion': '3',
+            'action':"search",
+            'type': 'rectangle',
+            'northeast': '(59.943921193288915, 10.826683044433594)',
+            'southwest': '(59.883683240905256, 10.650901794433594)',
+            'existingids':'189,195,199,89,48'},
+    dataType: 'json'
+});
+    
+    
+} );
+
